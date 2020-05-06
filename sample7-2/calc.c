8fcb73cb16dc72fe7dcc1734e6fb07e933fd0959
kimura121212 <ykimuranoi@gmail.com>
2020-05-06 13:16:36 +0900

    c言語 ファイル分割

---
+++ sample7-2/calc.c
@@ -0,0 +1,7 @@
+#include "calc.h"
+ 
+//  平均値求める関数
+double avg(double l,double m){
+    double r = (l + m) / 2.0;
+    return r;
+}
---
+++ sample7-2/calc.h
@@ -0,0 +1,8 @@
+// もし一回めcalc.hが定義されてなければ定義して下さい。定義されていたのならプロトタイプ宣言のところも一緒に飛ばしてください。
+#ifndef _CALC_H_
+#define _CALC_H_
+ 
+//  プロトタイプ宣言
+double avg(double,double);
+// hファイルを二回読み込むことを防ぐ
+#endif // _CALC_H_
