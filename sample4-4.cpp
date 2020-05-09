84ce185fa35273cdc29cb296c68db9dac7a0a074
kimura121212 <ykimuranoi@gmail.com>
2020-05-09 22:40:24 +0900

    newとdeleteと基本のデータ型2

---
+++ sample4-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int *p = 0;
+	int i;
+	p = new int[10];  // int型10個ぶんの領域を動的に確保
+	for (i = 0; i<10; ++i)
+	{
+		p[i] = i;
+		cout << p[i] << endl;
+	}
+	delete[] p;       // 動的に確保した領域を開放
+	return 0;
+}
